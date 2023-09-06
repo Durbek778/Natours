2bebfa828c8cd82fa272c6869f5b5f49dbca2fd5
Durbek <dergasbaev@gmail.com>
2023-09-07 00:12:27 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  console.log(newTour);
+  // res.send('Done  ');
+});
+const port = 3080;
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
